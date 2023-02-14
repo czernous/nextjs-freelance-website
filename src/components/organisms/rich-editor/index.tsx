5ed8b56d1d63e2c3d395255a9b29874273c15945
Anton Chernous <czernous_a@hotmail.com>
2023-02-14 15:19:22 +0400

    add ImageGallery to RichEditor

--- src/components/organisms/rich-editor/index.tsx
+++ src/components/organisms/rich-editor/index.tsx
@@ -2,6 +2,10 @@ import { useEditor, Editor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Image from '@tiptap/extension-image';
 import styles from './editor.module.scss';
+import { useEffect, useState } from 'react';
+import { IImage } from '@src/interfaces';
+import { handleGalleryOpen } from '../image-gallery/utils';
+import ImageGallery from '../image-gallery';
 
 interface ITipTapEditorProps {
   editor: Editor;
@@ -15,141 +19,177 @@ interface IEditorProps {
 const MenuBar = ({ ...props }: ITipTapEditorProps) => {
   const { editor } = props;
 
-  const addImage = () => {
-    const url = window.prompt('URL');
-
-    if (url) {
-      editor.chain().focus().setImage({ src: url }).run();
-    }
+  const [images, setImages] = useState<IImage[] | null>(null);
+  const [isGalleryOpen, setIsGalleryOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<IImage | null>(null);
+  /* istanbul ignore next */
+  const handleImageSelection = (image: IImage) => {
+    setSelectedImage(image);
+    setIsGalleryOpen(false);
   };
 
-  return (
-    <div className={styles.menuBar}>
-      <button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        disabled={!editor.can().chain().focus().toggleBold().run()}
-        className={editor.isActive('bold') ? styles.activeMenuBtn : ''}
-      >
-        bold
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        disabled={!editor.can().chain().focus().toggleItalic().run()}
-        className={editor.isActive('italic') ? styles.activeMenuBtn : ''}
-      >
-        italic
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleStrike().run()}
-        disabled={!editor.can().chain().focus().toggleStrike().run()}
-        className={editor.isActive('strike') ? styles.activeMenuBtn : ''}
-      >
-        strike
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleCode().run()}
-        disabled={!editor.can().chain().focus().toggleCode().run()}
-        className={editor.isActive('code') ? styles.activeMenuBtn : ''}
-      >
-        code
-      </button>
-      <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
-        clear marks
-      </button>
-      <button onClick={() => editor.chain().focus().clearNodes().run()}>
-        clear nodes
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setParagraph().run()}
-        className={editor.isActive('paragraph') ? styles.activeMenuBtn : ''}
-      >
-        paragraph
-      </button>
+  const addImage = () => {
+    handleGalleryOpen(setImages);
+    setIsGalleryOpen(true);
+  };
 
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        className={
-          editor.isActive('heading', { level: 2 }) ? styles.activeMenuBtn : ''
-        }
-      >
-        h2
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        className={
-          editor.isActive('heading', { level: 3 }) ? styles.activeMenuBtn : ''
-        }
-      >
-        h3
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-        className={
-          editor.isActive('heading', { level: 4 }) ? styles.activeMenuBtn : ''
-        }
-      >
-        h4
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-        className={
-          editor.isActive('heading', { level: 5 }) ? styles.activeMenuBtn : ''
-        }
-      >
-        h5
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-        className={
-          editor.isActive('heading', { level: 6 }) ? styles.activeMenuBtn : ''
-        }
-      >
-        h6
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive('bulletList') ? styles.activeMenuBtn : ''}
-      >
-        bullet list
-      </button>
-      <button onClick={addImage}>add image from URL</button>
-      <button
-        onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive('orderedList') ? styles.activeMenuBtn : ''}
-      >
-        ordered list
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        className={editor.isActive('codeBlock') ? styles.activeMenuBtn : ''}
-      >
-        code block
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBlockquote().run()}
-        className={editor.isActive('blockquote') ? styles.activeMenuBtn : ''}
-      >
-        blockquote
-      </button>
-      <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
-        horizontal rule
-      </button>
-      <button onClick={() => editor.chain().focus().setHardBreak().run()}>
-        hard break
-      </button>
-      <button
-        onClick={() => editor.chain().focus().undo().run()}
-        disabled={!editor.can().chain().focus().undo().run()}
-      >
-        undo
-      </button>
-      <button
-        onClick={() => editor.chain().focus().redo().run()}
-        disabled={!editor.can().chain().focus().redo().run()}
-      >
-        redo
-      </button>
-    </div>
+  useEffect(() => {
+    if (selectedImage)
+      editor.chain().focus().setImage({ src: selectedImage.secureUrl }).run();
+  }, [selectedImage, editor]);
+
+  return (
+    <>
+      <div className={styles.menuBar}>
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          disabled={!editor.can().chain().focus().toggleBold().run()}
+          className={editor.isActive('bold') ? styles.activeMenuBtn : ''}
+        >
+          bold
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          disabled={!editor.can().chain().focus().toggleItalic().run()}
+          className={editor.isActive('italic') ? styles.activeMenuBtn : ''}
+        >
+          italic
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          disabled={!editor.can().chain().focus().toggleStrike().run()}
+          className={editor.isActive('strike') ? styles.activeMenuBtn : ''}
+        >
+          strike
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleCode().run()}
+          disabled={!editor.can().chain().focus().toggleCode().run()}
+          className={editor.isActive('code') ? styles.activeMenuBtn : ''}
+        >
+          code
+        </button>
+        <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
+          clear marks
+        </button>
+        <button onClick={() => editor.chain().focus().clearNodes().run()}>
+          clear nodes
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setParagraph().run()}
+          className={editor.isActive('paragraph') ? styles.activeMenuBtn : ''}
+        >
+          paragraph
+        </button>
+
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          className={
+            editor.isActive('heading', { level: 2 }) ? styles.activeMenuBtn : ''
+          }
+        >
+          h2
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 3 }).run()
+          }
+          className={
+            editor.isActive('heading', { level: 3 }) ? styles.activeMenuBtn : ''
+          }
+        >
+          h3
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 4 }).run()
+          }
+          className={
+            editor.isActive('heading', { level: 4 }) ? styles.activeMenuBtn : ''
+          }
+        >
+          h4
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 5 }).run()
+          }
+          className={
+            editor.isActive('heading', { level: 5 }) ? styles.activeMenuBtn : ''
+          }
+        >
+          h5
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 6 }).run()
+          }
+          className={
+            editor.isActive('heading', { level: 6 }) ? styles.activeMenuBtn : ''
+          }
+        >
+          h6
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive('bulletList') ? styles.activeMenuBtn : ''}
+        >
+          bullet list
+        </button>
+        <button onClick={addImage}>add image from URL</button>
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive('orderedList') ? styles.activeMenuBtn : ''}
+        >
+          ordered list
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+          className={editor.isActive('codeBlock') ? styles.activeMenuBtn : ''}
+        >
+          code block
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          className={editor.isActive('blockquote') ? styles.activeMenuBtn : ''}
+        >
+          blockquote
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setHorizontalRule().run()}
+        >
+          horizontal rule
+        </button>
+        <button onClick={() => editor.chain().focus().setHardBreak().run()}>
+          hard break
+        </button>
+        <button
+          onClick={() => editor.chain().focus().undo().run()}
+          disabled={!editor.can().chain().focus().undo().run()}
+        >
+          undo
+        </button>
+        <button
+          onClick={() => editor.chain().focus().redo().run()}
+          disabled={!editor.can().chain().focus().redo().run()}
+        >
+          redo
+        </button>
+      </div>
+      {images && (
+        <ImageGallery
+          images={images}
+          isOpen={isGalleryOpen}
+          onClose={
+            /* istanbul ignore next */
+            () => setIsGalleryOpen(false)
+          }
+          onImageSelect={handleImageSelection}
+        />
+      )}
+    </>
   );
 };
 /* istanbul ignore next */
