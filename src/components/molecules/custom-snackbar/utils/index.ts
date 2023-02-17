7ef15e13491785d6e1fd891899f79eff2086b544
Anton Chernous <czernous_a@hotmail.com>
2023-02-17 15:34:52 +0400

    refactor updateSnackbarProps

--- src/components/molecules/custom-snackbar/utils/index.ts
+++ src/components/molecules/custom-snackbar/utils/index.ts
@@ -7,29 +7,28 @@ export const updateSnackbarProps = async (
 ) => {
   if (!r) return;
   const severity = r.status === 200 || r.status === 204 ? 'success' : 'error';
+  const hasJson = r.headers.get('Content-Type')?.includes('application/json');
 
   let statusMsg;
 
-  if (r.headers.get('Content-Type')?.includes('application/json')) {
-    try {
-      statusMsg = await r.json();
-    } catch (error) {
-      /* istanbul ignore next */
-      statusMsg = {
-        message:
-          statusMsg.message ||
-          (r.status === 200 ? 'Successfully saved data' : 'Error saving data'),
-      };
-    }
-  } else {
-    try {
-      statusMsg = {
-        message: r.status === 204 ? 'Successfully saved data' : await r.text(),
-      };
-    } catch (error) {
-      /* istanbul ignore next */
-      statusMsg = { message: 'Error reading response as text' };
-    }
+  try {
+    statusMsg = hasJson
+      ? await r.json()
+      : {
+          message:
+            r.status === 204 ? 'Successfully saved data' : await r.text(),
+        };
+  } catch (error) {
+    /* istanbul ignore next */
+    statusMsg = hasJson
+      ? {
+          message:
+            statusMsg.message ||
+            (r.status === 200
+              ? 'Successfully saved data'
+              : 'Error saving data'),
+        }
+      : { message: 'Error reading response as text' };
   }
 
   callback({
