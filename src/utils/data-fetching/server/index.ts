0716e12f03698ef6bc0192f4017bcc5e83b968fc
Anton Chernous <czernous_a@hotmail.com>
2023-03-21 21:04:19 +0400

    upd serverside fetch to throw when data is null

--- src/utils/data-fetching/server/index.ts
+++ src/utils/data-fetching/server/index.ts
@@ -88,6 +88,8 @@ export const serverSideBackendFetch = async <T>(
 
   const data: T = (await response?.json()) ?? null;
 
+  if (data === null) throw new Error('Error fetching data');
+
   return data;
 };
 export const handleServerError = (res: ServerResponse, error: IError) => {
