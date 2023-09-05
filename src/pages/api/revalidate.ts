9a2c3a327936f9e7760bd4bb09f2a16404be4b4b
Anton Chernous <czernous_a@hotmail.com>
2023-09-05 16:53:44 +0100

    cleanup

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -5,20 +5,19 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   // Check for secret to confirm this is a valid request
-  console.log('hitting revalidate');
+
   const path = req.query.path;
   if (!path) return res.status(400).json({ error: {} });
 
   try {
     // this should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-    console.log(path);
+
     await res.revalidate(path.toString());
     return res.json({ revalidated: true, now: Date.now() });
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
-    console.log(err);
     return res.status(500).send('Error revalidating');
   }
 }
