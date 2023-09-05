0d026850d9e6339b65a49b7ca59cfcdf6e3e2674
Anton Chernous <czernous_a@hotmail.com>
2023-09-05 12:44:00 +0100

    upd revalidate.ts

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,17 +1,24 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { revalidatePath } from 'next/cache';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export async function POST(request: NextRequest) {
-  const path = request.nextUrl.searchParams.get('path');
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  // Check for secret to confirm this is a valid request
+  console.log('hitting revalidate');
+  const path = req.query.path;
+  if (!path) return res.status(400).json({ error: {} });
 
-  if (!path) {
-    return NextResponse.json(
-      { message: 'Missing path param' },
-      { status: 400 },
-    );
+  try {
+    // this should be the actual path not a rewritten path
+    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+    console.log(path);
+    await res.revalidate(path.toString());
+    return res.json({ revalidated: true, now: Date.now() });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    console.log(err);
+    return res.status(500).send('Error revalidating');
   }
-
-  revalidatePath(path);
-
-  return NextResponse.json({ revalidated: true, now: Date.now() });
 }
